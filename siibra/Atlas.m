e9b6e33afeb2424805dcf88ecb7d0b6b9133e708
valentin-pinkau <valentin.pinkau@scalableminds.com>
2022-01-17 13:49:11 +0100

    replace api_call with webread

--- siibra/Atlas.m
+++ siibra/Atlas.m
@@ -9,7 +9,7 @@ classdef Atlas
             atlas.name = name;
         end
         function parcellation = getParcellation(obj, parcellation_name_query)
-            parcellations = api_call(Siibra.apiEndpoint + "atlases/" + obj.id + "/parcellations");
+            parcellations = webread(Siibra.apiEndpoint + "atlases/" + obj.id + "/parcellations");
             for percellation_row = 1:numel(parcellations)
                 if parcellation_name_query == parcellations(percellation_row).name
                     parcellation = Parcellation(parcellations(percellation_row).id, parcellations(percellation_row).name, obj.id);

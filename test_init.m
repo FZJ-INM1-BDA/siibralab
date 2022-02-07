fde331bd9366fe25058d986c930d04cc6f143b1e
valentin-pinkau <valentin.pinkau@scalableminds.com>
2022-02-07 09:18:29 +0100

    remove test

--- test_init.m
+++
@@ -1,6 +0,0 @@
-siibra = Siibra();
-human_atlas = siibra.atlases(1);
-parcellation = human_atlas.Parcellations(1);
-plot(parcellation.Graph, 'Layout','force')
-region = parcellation.getRegion("Ch 123 (Basal Forebrain) left");
-spaces = region.SpacesToUrl.keys;

2dd5349ebeef1623fad07c5bbff39aa8e57f6832
valentin-pinkau <valentin.pinkau@scalableminds.com>
2022-08-29 17:35:36 +0200

    add changes in fuzzyMatching

--- +siibra/+internal/fuzzyMatching.m
+++ +siibra/+internal/fuzzyMatching.m
@@ -6,16 +6,32 @@ end
 
 %FUZZY_MATCHING Summary of this function goes here
 %   Detailed explanation goes here
-    difflib = py.importlib.import_module('difflib');
+    % If python is available use difflib to rank values in haystack based
+    % on similarity. If python is not available check if the query is a
+    % substring and return the first elementIndex for which this is true.
 
     lowerQuery = lower(query);
     lowerHaystack = lower(haystack);
-    
-    python_matched_names = difflib.get_close_matches(lowerQuery, cellstr(lowerHaystack), py.int(1), 0.3);
-    matched_names = cellfun(@string,cell(python_matched_names),'UniformOutput',false);
-    if isempty(matched_names)
-        error ("Empty result for query " + query);
+
+    if isempty(pyenv().Version)
+        % no python available
+        matchedIndices = find(contains(lowerHaystack, lowerQuery));
+        if isempty(matchedIndices)
+            error (strcat("Empty result for query ", query, " in ", sprintf("%s", haystack + ", ")));
+        end
+        matchedIndex = matchedIndices(1);
+            
+    else
+        difflib = py.importlib.import_module('difflib');
+
+        python_matched_names = difflib.get_close_matches(lowerQuery, cellstr(lowerHaystack), py.int(1), 0.3);
+        matched_names = cellfun(@string,cell(python_matched_names),'UniformOutput',false);
+        if isempty(matched_names)
+            error (strcat("Empty result for query ", query, " in ", sprintf("%s", haystack + ", ")));
+        end
+        matchedIndex = find(ismember(lowerHaystack, matched_names{1}));
+
     end
-    matchedIndex = find(ismember(lowerHaystack, matched_names{1}));
+    
 end
 
